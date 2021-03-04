5a0f19e2bd50c971798d4b39cb59b37c7a2c65a5
Daniel Mack <daniel@zonque.org>
2021-03-04 12:22:31 +0100

    Add flags definitions

--- types.go
+++ types.go
@@ -76,3 +76,50 @@ const (
 	// InterfaceUnspec - Unspecified/all interface(s)
 	InterfaceUnspec = -1
 )
+
+const (
+	// PublishUnique - The RRset is intended to be unique
+	PublishUnique = 1
+	// PublishNoProbe - Though the RRset is intended to be unique no probes shall be sent
+	PublishNoProbe = 2
+	// PublishNoAnnouce - Do not announce this RR to other hosts
+	PublishNoAnnouce = 4
+	// PublishAllowMultiple - Allow multiple local records of this type, even if they are intended to be unique
+	PublishAllowMultiple = 8
+	// PublishNoReverse - don't create a reverse (PTR) entry
+	PublishNoReverse = 16
+	// PublishNoCookie - do not implicitly add the local service cookie to TXT data
+	PublishNoCookie = 32
+	// PublishUpdate - Update existing records instead of adding new ones
+	PublishUpdate = 64
+	// PublishUseWideArea - Register the record using wide area DNS (i.e. unicast DNS update)
+	PublishUseWideArea = 128
+	// PublishUseMulticast - Register the record using multicast DNS
+	PublishUseMulticast = 256
+)
+
+const (
+	// LookupUseWideArea - Force lookup via wide area DNS
+	LookupUseWideArea = 1
+	// LookupUseMulticast - Force lookup via multicast DNS
+	LookupUseMulticast = 2
+	// LookupNoTXT - When doing service resolving, don't lookup TXT record
+	LookupNoTXT = 4
+	// LookupNoAddreess - When doing service resolving, don't lookup A/AAAA record
+	LookupNoAddreess = 8
+)
+
+const (
+	// LookupResultCached - This response originates from the cache
+	LookupResultCached = 1
+	// LookupResultWideArea - This response originates from wide area DNS
+	LookupResultWideArea = 2
+	// LookupResultMulticast  - This response originates from multicast DNS
+	LookupResultMulticast = 4
+	// LookupResultLocal - This record/service resides on and was announced by the local host. Only available in service and record browsers and only on AVAHI_BROWSER_NEW.
+	LookupResultLocal = 8
+	// LookupResultOurOwn - This service belongs to the same local client as the browser object. Only available in avahi-client, and only for service browsers and only on AVAHI_BROWSER_NEW.
+	LookupResultOurOwn = 16
+	// LookupResultStatic - The returned data has been defined statically by some configuration option
+	LookupResultStatic = 32
+)
